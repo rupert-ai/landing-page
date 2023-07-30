0ee8f9737d40db287e94e234398f421cb6599d96
Revuocvan <revuocvan@gmail.com>
2023-07-31 01:35:15 +0300

    added dummy loading

--- src/components/LoadingImage/LoadingImage.component.tsx
+++ src/components/LoadingImage/LoadingImage.component.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import "./LoadingImage.styles.scss";
+import loaderGif from "../../assets/gifs/loading.gif";
 
 interface LoadingImageProps {
     src: string;
@@ -10,19 +12,21 @@ const LoadingImage: React.FC<LoadingImageProps> = ({ src, alt, className }) => {
     const [isLoading, setIsLoading] = useState(true);
 
     const handleImageLoad = () => {
-        setIsLoading(false);
+        const delay = Math.random() * (1500 - 500) + 500;
+
+        setTimeout(() => {
+            setIsLoading(false);
+        }, delay);
     };
 
     return (
-        <div>
-            {isLoading && <div className="text-1">Loading...</div>}
-            <img
-                src={src}
-                alt={alt}
-                className={className}
-                onLoad={handleImageLoad}
-                style={{ display: isLoading ? "none" : "inline"}}
-            />
+        <div className="loading-image">
+            {isLoading && (
+                <div className="image-loader">
+                    <img alt="Loading..." src={loaderGif} className="image-loader-gif" />
+                </div>
+            )}
+            <img src={src} alt={alt} className={className} onLoad={handleImageLoad} style={{ display: isLoading ? "none" : "inline" }} />
         </div>
     );
 };
