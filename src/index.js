2cebd8126e1de809b417fd38ea0887fae9875d16
Revuocvan <revuocvan@gmail.com>
2023-09-23 11:12:35 +0300

    added gtm

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+    gtmId: 'GTM-TQD5Z2S',
+};
+TagManager.initialize(tagManagerArgs);
 
 const router = createBrowserRouter([
     {
-        path: "/*",
+        path: '/*',
         element: <App />,
     },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <RouterProvider router={router} />
     </React.StrictMode>
